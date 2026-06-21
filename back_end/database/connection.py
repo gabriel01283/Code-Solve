a4abf90d18d4594bff954f733721abf7dc4c84ca
gabriel01283 <gabrielsaraivasampaio13@gmail.com>
2026-06-21 17:47:20 -0300

    Configurando conexao do banco por variaveis de ambiente

--- back_end/database/connection.py
+++ back_end/database/connection.py
@@ -1,10 +1,21 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+
+load_dotenv(encoding="utf-8")
+
 
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg2.connect(database_url)
+
     return psycopg2.connect(
-        database="solve-code",
-        user="postgres",
-        password="7777",
-        host="localhost",
-        port="5432"
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
